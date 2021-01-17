2d57fc56eb643acabf9028e573b3609c3be3fdc5
Abdrr97 <abdrr97@gmail.com>
2021-01-17 21:05:11 +0100

    fixing some issues

--- server/server.js
+++ server/server.js
@@ -1,17 +1,18 @@
 const express = require('express')
 const cors = require('cors')
 const monk = require('monk')
-
+const Filter = require('bad-words')
+const rateLimit = require('express-rate-limit')
 // mongo will automatically create db or collection if not exist 
-const db = monk('localhost/mini_twitter') // initialising our local mongo db named 'mini_twitter'
+const db = monk(process.env.MONGO_URI || 'localhost/mini_twitter') // initialising our local mongo db named 'mini_twitter'
 const tweets = db.get('tweets') // collection inside of our local mongo db
-
 const app = express()
 const PORT = 5000
-
+const filter = new Filter()
 
 app.use(cors()) // cors works as middleware to prevent 'CORS ORIGIN ERROR'
 app.use(express.json()) // this allow upcoming requests to read json data
+
 
 app.listen(PORT, () => {
     console.log('Listening on port 5000');
@@ -32,13 +33,17 @@ app.get('/tweets', (req, res) => {
         })
 
 })
-
+// limit users from spamming
+app.use(rateLimit({
+    windowMs: 30 * 1000,
+    max: 1
+}))
 app.post('/tweets', (req, res) => {
     if (validated(req.body)) {
         // insert to mongo-db
         let tweet = {
-            name: req.body.name.toString(),
-            message: req.body.message.toString(),
+            name: filter.clean(req.body.name.toString()),
+            message: filter.clean(req.body.message.toString()),
             created_at: new Date()
         }
 
